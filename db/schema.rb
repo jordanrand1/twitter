66acbbab32f8f529dd3493ecc27cf0bcdc6d15d9
MrTj458 <mrtj458@gmail.com>
2018-09-08 10:03:00 -0600

    Added comments model correctly

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_08_155607) do
+ActiveRecord::Schema.define(version: 2018_09_08_160220) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,16 @@ ActiveRecord::Schema.define(version: 2018_09_08_155607) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_bios_on_user_id"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text "message"
+    t.bigint "user_id"
+    t.bigint "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -45,5 +55,7 @@ ActiveRecord::Schema.define(version: 2018_09_08_155607) do
   end
 
   add_foreign_key "bios", "users"
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "posts", "users"
 end
